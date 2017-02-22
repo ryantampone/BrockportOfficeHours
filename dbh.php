41c34f60fb91f2a079dbfebd096945830a151c16
Ryan Tampone <rtamp1@brockport.edu>
2017-02-21 23:18:05 -0500

    Delete dbh.php

--- dbh.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost", "bportofficehours", "bportofficehours", "brockportofficehours");
-
-if (!$conn) {
-  die("Connection failed: ".mysqli_connect_error());
-}
-
-?>
