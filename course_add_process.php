557e75fb519e966b6d2a4ee90a6be104b551f1b0
Adam <asmit26@brockport.edu>
2017-03-02 18:13:54 -0500

    AJAX DONE
    Finished ajax for add course. Selecting the department populates the
    faculty name field of that department. FINALLY

--- course_add_process.php
+++ course_add_process.php
@@ -2,27 +2,24 @@
   require('db_cn.inc');
   connect_and_select_db(DB_SERVER, DB_UN, DB_PWD,DB_NAME);
 
-  $selected_dept = $_REQUEST['deptcode'];
-  $sql_fac_id = "SELECT * FROM Faculty WHERE DepartmentID =
-    (SELECT DepartmentID FROM Department WHERE Code = '$selected_dept')";
-  $result = mysql_query($sql_fac_id);
+  $selected_dept = $_POST['deptcode'];
+  $sql_fac_id = "SELECT NetID, FirstName, LastName FROM Faculty WHERE DepartmentID =
+    (SELECT DepartmentID FROM Department WHERE Code = '$selected_dept') ORDER BY LastName";
+  $result = mysql_query($sql_fac_id); //$result = resource id
 
   while($row = mysql_fetch_assoc($result))
   {
     $netid = $row["NetID"];
     $fn = $row["FirstName"];
     $ln = $row["LastName"];
+    echo json_encode($row)."\n";
   }
 
-  if(isset($netid))
-  {
-    $netid = $_POST["NetID"];
-    $fn = $_POST["FirstName"];
-    $ln = $_POST["LastName"];
-  }
-  $arr = mysql_fetch_row($result);
+  /*$netid = $_GET["netid"];
+  $fn = $_GET["fn"];
+  $ln = $_GET["ln"];*/
 
-  echo json_encode($arr);
+  //echo $netid.", ".$fn.", ".$ln;
 
 
   /*if(!$result)
--- scripts/course_add_ajax.js
+++ scripts/course_add_ajax.js
@@ -1,23 +1,63 @@
+function formattedJSON (str)
+{
+  var newStr = str.split("\n");
+  return newStr;
+}
+
 function updateFaculty()
 {
   var dept = $("#deptcode").val();
-
   $.post("course_add_process.php",
     {
       deptcode: dept
     },
     function(data)
     {
-      // Can't process info here (asynchronous)
-      alert("Post successful.\n"+data+"\nCalling process_query...");
-      process_query(data);
+      // newobject to communicate with course_add_process.php
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function()
+      {
+        // if request is done
+        if(xhr.readyState === 4)
+        {
+          // if request is successful
+          if(xhr.status === 200)
+          {
+            // Reached here if successful
+            // data = {"NetID:","fjones","FirstName:","Faculty" ...}
+            if (data != "")
+              process_query(data);
+            else alert("There are no faculty members saved in this department.");
+          }
+        }
+      };
+      xhr.open("get", "course_add_process.php", true);
+      xhr.send();
     });
 
 }
 
-function process_query(data)
+function process_query(j)
 {
-  alert("Net ID = "+data[0]+"\nFirst Name = "+data[1]+"\nLast Name = "+data[2]);
+  // str is formatted to split the responseText input in individual entries
+  // from the database and put into an array
+  var str = formattedJSON(j);
+
+  //empty the Faculty Name drop down list before populating it with data retrieved
+  $("#netid").empty();
+
+  // For each entry retrieved from database
+  for (var i = 0; i < str.length-1; i++)
+  {
+    // Get each key,value pair in entry i
+    var nextProf = JSON.parse(str[i]);
+    var netid = nextProf.NetID;
+    var fn = nextProf.FirstName;
+    var ln = nextProf.LastName;
+
+    // add the option to the Faculty Name drop down list
+    $("#netid").append("<option value='" + netid + "'>" + ln + ", " + fn + "</option>");
+  }
 }
   /*$.ajax({
     url: "course_add_process.php",
